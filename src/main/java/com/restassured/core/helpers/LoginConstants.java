cfce172c9b783c68b9e1c3e032b1a13edddee198
venkata.reddy <venkata.reddy@harness.io>
2023-02-22 18:46:55 +0530

    adding the additional support

--- src/main/java/com/restassured/core/helpers/LoginConstants.java
+++ src/main/java/com/restassured/core/helpers/LoginConstants.java
@@ -1,5 +1,8 @@
 package com.restassured.core.helpers;
 
 public class LoginConstants {
-    public static final String login="/token";
+    public static final String login="/v2-beta/token";
+    public static final String CREATE_STACK="/v2-beta/projects/{projectId}/stack";
+    public static final String CREATE_STACK_JSON= "/src/main/resources/common/createStack.json";
+
 }
